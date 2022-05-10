9b979f75814ec62bb9eeed8c993401d456069e51
AhmedEmadh <104726251+AhmedEmadh@users.noreply.github.com>
2022-05-11 00:51:07 +0200

    added more functions

--- main.c
+++ main.c
@@ -106,6 +106,7 @@ void lcd_print_int_row(int n,int row){}
 void lcd_clear(){}
 int keypad_input(){}
 int pushed_key(){}
+void pop_count_down(){}
 void beef_count_down(){}
 void chicken_count_down(){}
 void start_cooking(){}
