2cf47bff33f58d2a5285f2c8602ae955f5828204
AhmedAbdElmotelbAli <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2022-05-11 08:52:16 +0200

    Update key.c

--- key.c
+++ key.c
@@ -1,24 +1,4 @@
-#include "D:\UV4\tm4c123gh6pm.h"
-#include "TM4C123.h" // Device header
-#define LCD_DATA GPIOB
-#define LCD_CTRL GPIOA
-#define RS 0x20 /* PORTA BIT5 mask */
-#define RW 0x40 /* PORTA BIT6 mask */
-#define EN 0x80 /* PORTA BIT7 mask */
-#include <stdint.h>
-#include "D:\io.h"
-
-void delayMs(int n);
-void delayUs(int n);
-void LCD_command(unsigned char command);
-void LCD_data(unsigned char data);
-void LCD_init(void);
- 
-void keypad_init(void);
-unsigned char keypad_kbhit(void);
-void keypad_init(void);
-unsigned char keypad_getkey(void);
- 
+#include "D:\EE319Kware\InputOutput_4C123\key\key.h"
 int main(void)
 {
 unsigned char key;
