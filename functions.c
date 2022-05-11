f5a77b469b65d873226dd6635a3d195ed08afd88
AhmedEmadh <104726251+AhmedEmadh@users.noreply.github.com>
2022-05-11 09:04:24 +0200

    All Functions  are Seperated in functions.c file to work and push on it

---
+++ functions.c
@@ -0,0 +1,16 @@
+#include "functions.h"
+void initialization(){}
+void lcd_print_char(char *p){}
+void lcd_print_int(int n){}
+void lcd_print_char_row(char *p,int row){}
+void lcd_print_int_row(int n,int row){}
+void lcd_clear(){}
+int keypad_input(){}
+int pushed_key(){}
+void pop_count_down(){}
+void beef_count_down(){}
+void chicken_count_down(){}
+void start_cooking(){}
+void stop_cooking(){}
+void delay_sec(int t){}
+void delay_ms(int t){}
