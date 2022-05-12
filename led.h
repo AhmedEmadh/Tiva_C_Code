fe478fa8fd2360dca4ad5a31d418711ecb9eaea2
ahmedadelhassan159 <104680320+ahmedadelhassan159@users.noreply.github.com>
2022-05-12 07:50:56 +0200

    Add files via upload
    led

---
+++ led.h
@@ -0,0 +1,4 @@
+void led_init(unsigned char port_name,unsigned char pin_number);
+void led_on(unsigned char port_name,unsigned char pin_number);
+void led_off(unsigned char port_name,unsigned char pin_number);
+void Led_TOG_bit(unsigned char port_name,unsigned char pin_number);
