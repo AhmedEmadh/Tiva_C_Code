6eef25d33bf7e45bc0d3334e41c4c8058ea6d357
ahmedadelhassan159 <104680320+ahmedadelhassan159@users.noreply.github.com>
2022-05-12 07:53:20 +0200

    Update led.c

--- led.c
+++ led.c
@@ -1,18 +1,18 @@
-#include "D:\EE319Kware\InputOutput_4C123\key\digital_input_output.h"
-#include "D:\EE319Kware\InputOutput_4C123\key\led.h"
+#include "digital_input_output.h"
+#include "led.h"
 void led_init(unsigned char port_name,unsigned char pin_number)
 {
-		void port_init(unsigned char port_name)
-		void digital_input_output_setpin_direction(port_name,pin_number, 0 )
+		void port_init(unsigned char port_name);
+		void digital_input_output_setpin_direction(port_name,pin_number, 0 );
 }
 void led_on(unsigned char port_name,unsigned char pin_number)
 {
-	void digital_input_output_VWite_pin(port_name,pin_number,1 )
+	void digital_input_output_VWite_pin(port_name,pin_number,1 );
 }
 
 void led_off(unsigned char port_name,unsigned char pin_number)
 {
-	void digital_input_output_VWite_pin(port_name,pin_number,0 )
+	void digital_input_output_VWite_pin(port_name,pin_number,0 );
 }
 void Led_TOG_bit(unsigned char port_name,unsigned char pin_number)
 {
