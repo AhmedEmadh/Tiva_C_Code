8637397db92fe36e565829118908bca6a30a631d
AhmedAbdElmotelbAli <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2022-05-13 13:50:22 +0200

    Update digital_input_output.h

--- digital_input_output.h
+++ digital_input_output.h
@@ -1,4 +1,4 @@
-#include "key.h"
+#include "tm4c123gh6pm.h"
 #define 	set_bit(name_register,pin_bit)  name_register |=(1<<pin_bit)
 #define 	CLR_bit(name_register,pin_bit)  name_register &= ~(1<<pin_bit)
 #define 	TOG_bit(name_register,pin_bit)  name_register ^=(1<<pin_bit)
