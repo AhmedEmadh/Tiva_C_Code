1d8e2e0072f9e917c5f4526006f1d73647d4fbd5
AhmedAbdElmotelbAli <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2022-05-14 03:26:07 +0200

    Update key.c

--- key.c
+++ key.c
@@ -1,8 +1,8 @@
 #include "digital_input_output.h"
 #include "tm4c123gh6pm.h"
-
+//PORT D ROW 0,1,2,3  C COLUM 4,5,6,7
 void key_pad_init(void);
-unsigned char keypad_read()
+unsigned char keypad_read(void)
 {
 
 const unsigned char keymap[4][4] = 
@@ -12,34 +12,76 @@ const unsigned char keymap[4][4] =
 { '7', '8', '9', 'C'},
 { '*', '0', '#', 'D'},
 };
-char row ,col ,x;
-char return_value=0xff;
-for(row=0;row<4;row++){
-digital_input_output_Vlowlabel('B', 0xff);
-digital_input_output_VWite_pin('B',row,0);
-for(col=4;col<8;col++)
+int Col,Row;
+digital_input_output_VWite_port('D',0);
+Col=(GPIO_PORTC_DATA_R&0xf0);
+if(Col == 0xf0) return 0; //NO PRESSED
+while(1){
+	Row=0;
+	digital_input_output_VWite_port('D',0x0E);
+	delay_us(4);
+	Col=(GPIO_PORTC_DATA_R&0xf0);
+	if(Col != 0xf0)break;
+		Row=1;
+	digital_input_output_VWite_port('D',0x0D);
+		delay_us(4);
+	Col=(GPIO_PORTC_DATA_R&0xf0);
+	if(Col != 0xf0)break;
+		Row=2;
+	digital_input_output_VWite_port('D',0x0B);
+		delay_us(4);
+	Col=(GPIO_PORTC_DATA_R&0xf0);
+	if(Col != 0xf0)break;
+		Row=3;
+	digital_input_output_VWite_port('D',0x07);
+		delay_us(4);
+	Col=(GPIO_PORTC_DATA_R&0xf0);
+	if(Col != 0xf0)break;
+	return 0;
+}
+if(Col==0xE0)
 {
-	x= digital_input_output_U8Read_pin('B',(col));
-	if(x==0)
+	return keymap[Row][0];
+}
+else if(Col==0xD0)
 {
-	return_value=keymap[row][col];
-	break;
+		return keymap[Row][1];
 }
-if(x==0)break;
-return return_value;
+else if(Col==0xB0)
+{
+		return keymap[Row][2];
 }
-	
+else if(Col==0x70)
+{
+		return keymap[Row][3];
+}
+else
+{
+	return 0;
 }
 }
+unsigned char key_pad_pressed(void)
+{
+int Col;
+digital_input_output_VWite_port('D',0);
+Col=(GPIO_PORTC_DATA_R&0xf0);
+if(Col == 0xf0) return 0;//not pressed
+else return 1; //key pressed
+}	
 void key_pad_init(void)
 {
-	port_init('B');
-	GPIO_PORTB_DIR_R=0x0f;
+	// ROW FOR PORTD
+	port_init('d');
+	GPIO_PORTD_DIR_R=0x0f;
+	GPIO_PORTD_ODR_R=0X0F;
+	//colum
+	port_init('C');
+	GPIO_PORTC_DIR_R=0XF0;
 	//do PUP GPIOB pin 4,5,6,7 col 1,2,3,4
-	digital_input_output_VEnablePUR('B',4); 
-	digital_input_output_VEnablePUR('B',5);
-	digital_input_output_VEnablePUR('B',6);
-	digital_input_output_VEnablePUR('B',7);
+	digital_input_output_VEnablePUR('C',4); 
+	digital_input_output_VEnablePUR('C',5);
+	digital_input_output_VEnablePUR('C',6);
+	digital_input_output_VEnablePUR('C',7);
 
 }
 unsigned char Keypad_Read()
@@ -47,5 +89,4 @@ unsigned char Keypad_Read()
 			key_pad_init();
 		return	keypad_read()	;
 }
-
 
