dd34ddb05439cebd825a1c83a93b3644e6437bca
ahmedadelhassan159 <104680320+ahmedadelhassan159@users.noreply.github.com>
2022-05-16 17:43:55 +0200

    LCD Configuration

--- LCD.h
+++ LCD.h
@@ -2,9 +2,9 @@
 #define __LCD_H__
 #include <stdio.h>          // printf, scanf, NULL 
 #include <stdlib.h>         // malloc, free, rand 
-#include "D:\UV4\tm4c123gh6pm.h"
-#include "D:\EE319Kware\InputOutput_4C123\project\Delay.h"
-#include "D:\EE319Kware\InputOutput_4C123\project\DMC1610A.h"
+#include "tm4c123gh6pm.h"
+#include "Delay.h"
+#include "DMC1610A.h"
 #define  time  500
 void LCD_Init              (void);
 void LCD_command           (unsigned int);
