7f60a1951d90bfd12b7011d6135d03b0c55f2a83
AhmedAbdElmotelbAli <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2022-05-16 17:52:54 +0200

    keypad configuration

--- keypad.h
+++ keypad.h
@@ -1,7 +1,7 @@
 #ifndef __KEYPAD_H__
 #define __KEYPAD_H__
-#include "D:\UV4\tm4c123gh6pm.h"
-#include "D:\EE319Kware\InputOutput_4C123\project\Delay.h"
+#include "tm4c123gh6pm.h"
+#include "Delay.h"
 #define  padRows  4
 #define  padCols  4
 
