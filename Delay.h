41bf8137a3ecebae2872bd687e6da7e0dfdfaa74
AhmedAbdElmotelbAli <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2022-05-19 11:59:28 +0200

    Update Delay.h

--- Delay.h
+++ Delay.h
@@ -1,8 +1,8 @@
 #ifndef __Delay_H__
 #define __Delay_H__
 
-void delay_ms(int);
-void delay_us(int);
+void delay_ms(int); // delay in micro second
+void delay_us(int); // delay in mile second
 
 #endif
 
