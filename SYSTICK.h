5c625d5c6d4d46cd9c3701b4ee12134df1e2a31b
AhmedAbdElmotelbAli <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2022-05-19 12:01:37 +0200

    Update SYSTICK.h

--- SYSTICK.h
+++ SYSTICK.h
@@ -4,8 +4,8 @@
 #define  NVIC_ST_CTRL_R        (*((volatile unsigned long *)0xE000E010))
 #define  NVIC_ST_RELOAD_R      (*((volatile unsigned long *)0xE000E014))
 #define  NVIC_ST_CURRENT_R     (*((volatile unsigned long *)0xE000E018))
-void SysTick_wait(unsigned long delay);
-void delayMs(int n);
-void delayUs(int n);
+void SysTick_wait(unsigned long delay); // delay more than mile second
+void delayMs(int n); // delay in  mile second
+void delayUs(int n); // delay in  micro second
 #endif
 
