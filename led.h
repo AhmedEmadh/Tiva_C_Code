c9680e4315c3303dc3963ea21327a86b3684dc10
AhmedAbdElmotelbAli <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2022-05-19 11:48:37 +0200

    Update led.h

--- led.h
+++ led.h
@@ -1,5 +1,10 @@
+#ifndef __led_H__
+#define __led_H__
 #include "digital_input_output.h"
 void led_init(unsigned char port_name,unsigned char pin_number);
 void led_on(unsigned char port_name,unsigned char pin_number);
 void led_off(unsigned char port_name,unsigned char pin_number);
 void Led_TOG_bit(unsigned char port_name,unsigned char pin_number);
+#endif
+
+
