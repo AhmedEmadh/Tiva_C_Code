43001a0976e73850066462e863204a615293e1b0
AhmedAbdElmotelbAli <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2022-05-21 13:12:24 +0200

    Update main.c

--- main.c
+++ main.c
@@ -97,11 +97,11 @@ int main() {
            
             do{
                 lcd_print_str("Cooking Time?");
-                delay_sec(2);
+                delay_ms(2000);
                 lcd_clear();
-                SW2_In = SW2_Input();
                 while(1){
                     input = keypad_switch_input();
+				            delay_ms(2000);		  
                     if(input == 'S'){break;}//if SW2 pressed give ok to the number
                     if (input == 'H'){goto other_start;}//if SW1 pressed clear the LCD and start the case again
                     temp[3] = input;//if other button pressed start the putting it in the screen by converting it into integer
@@ -110,26 +110,30 @@ int main() {
                     temp[0] = inputs[1];//temp here is ready to be cloned by input
                     for(i=0;i<=3;i++){inputs[i] = temp[i];}//put inputs[] = temp[] (cloning)
                     displaytime_char(inputs[0],inputs[1],inputs[2],inputs[3]);
+				            delay_ms(2000);
+										SW2_In = SW2_Input();										
+										if(SW2_In==0x01){goto x;}
                 }//while SW2 not pressed
-            }while((char_to_int(inputs[0]) >= 30) || (char_to_int(inputs[0]) == 0));//if minites >= 30 or equals 0 ==> reject and loop again 
-            //if time allowed convert it to secounds and count down 
+            }while(1);
+
+						x:
             time_seconds_other = inputs_to_seconds (inputs[0],inputs[1],inputs[2],inputs[3]);
             other_count_down(time_seconds_other);
             state = not_cooking;
             break;
         case error_beef: //added error beef
             lcd_print_str("Err");
-            delay_sec(2);
+	          delay_ms(2000);		  
             state = beef; //previous state
             break;
         case error_chicken: // added error chicken
             lcd_print_str("Err");
-            delay_sec(2);
+	          delay_ms(2000);		  
             state = chicken; //previous state
             break;
         case error_other: // added error chicken
             lcd_print_str("Err");
-            delay_sec(2);
+	          delay_ms(2000);		  
             state = chicken; //previous state
             break;
         default:
