8fd56fca598bcd3d76f7d340552526275de5a53b
AhmedAbdElmotelbAli <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2022-05-23 19:06:12 +0200

    Update interrupt.h

--- interrupt.h
+++ interrupt.h
@@ -1,5 +1,8 @@
 #ifndef __interrupt_H__
 #define __interrupt_H__
+#define PD0 (*((volatile uint32_t *)0x40007004))
 void edge_counter_init(void);
 void GPIOF_Handler(void);
+void SW_PORTE(void);
+void GPIOPortE_Handler(void);
 #endif
