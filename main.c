a184bffdd653e74e5d86c35f7d5df285ad9fd437
AhmedAbdElmotelbAli <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2022-05-24 22:11:59 +0200

    Update main.c

--- main.c
+++ main.c
@@ -42,47 +42,47 @@ int main() {
             lcd_print_str("popcorn");//print sting popcorn
             delay_ms(2000);//delay for 2 s
 	    while(1){
-	    	lcd_clear();
-           	lcd_print_str("please press sw2");
-            	delay_ms(2000);
+	    	lcd_clear();//clear lcd
+           	lcd_print_str("please press sw2");//string to start
+            	delay_ms(2000);//delay
          	if(GPIO_PORTF_DATA_R==0x10){break;}
 		}
-		lcd_clear();
+		lcd_clear();//clear lcd
     		pop_count_down(1); //count from 60 s to 0
             	lcd_clear();
-            	lcd_print_str("End popcorn");
-            	delay_ms(2000);
+            	lcd_print_str("End popcorn");//print string
+            	delay_ms(2000);//delay
 		led(); // blink the LEDs
-		GPIO_PORTD_DATA_R|=0X01;
-                delay_ms(2000);
-		GPIO_PORTD_DATA_R|=0X00;
+		GPIO_PORTD_DATA_R|=0X01;//sound on
+                delay_ms(2000);//delay
+		GPIO_PORTD_DATA_R|=0X00;//sound
 		state = not_cooking;
         break;
         case beef:
-            lcd_print_str("Beef weight?");
-            delay_ms(2000);
-            input = keypad_input();
+            lcd_print_str("Beef weight?");//print string
+            delay_ms(2000);//delay
+            input = keypad_input();//take form keypad
 				
             if ( !(input == 'A' || input =='B' || input =='C' || input =='D' || input =='*' || input =='#' || input =='0') )//changed smalles to Capitals
             {   //Valid number
                 weight = input - '0';
-                lcd_print_int(weight);
-                delay_sec (2);
-                lcd_clear();
+                lcd_print_int(weight);//print
+                delay_sec (2);//delay 2 s
+                lcd_clear();//clear lcd
 		while(1){
-	    	     lcd_clear();
-            	     lcd_print_str("please press sw2");
-                     delay_ms(2000);
+	    	     lcd_clear();//clear lcd
+            	     lcd_print_str("please press sw2");//print string
+                     delay_ms(2000);//delay
          	     if(GPIO_PORTF_DATA_R==0x10){break;}
 		}
                 beef_count_down(weight); // starts a countdown with a time = 30s * weight
-                lcd_clear();
-                lcd_print_str("End Beef weight");
-                delay_ms(2000);
-                lcd_clear();
-		GPIO_PORTD_DATA_R|=0X01;
-                delay_ms(2000);
-		GPIO_PORTD_DATA_R|=0X00;
+                lcd_clear();//clear lcd
+                lcd_print_str("End Beef weight");//print String 
+                delay_ms(2000);//delay
+                lcd_clear();//clear lcd
+		GPIO_PORTD_DATA_R|=0X01;//start sound
+                delay_ms(2000);//delay
+		GPIO_PORTD_DATA_R|=0X00;//stop sound
 		led();
                 state = not_cooking;
             }
@@ -92,31 +92,31 @@ int main() {
             }
             break;
         case chicken:
-            lcd_print_str("chicken weight?");
-            delay_ms(2000);
-            input = keypad_input();
+            lcd_print_str("chicken weight?");//print string
+            delay_ms(2000);//delay
+            input = keypad_input();// take for keybad
             if (!(input == 'A' || input =='B' || input =='C' || input =='D' || input =='*' || input =='#' || input =='0'))//changed smalles to Capitals
             {//valid number
             weight = input - '0';
-            lcd_print_int(weight);
-            delay_sec (2);
-            lcd_clear();
-            lcd_print_int(weight);
+            lcd_print_int(weight);//print string
+            delay_sec (2);//delay
+            lcd_clear();//clear lcd
+            lcd_print_int(weight);//print string
 	    while(1){
-		lcd_clear();
-            	lcd_print_str("please press sw2");
-            	delay_ms(2000);
+		lcd_clear();//clear lcd
+            	lcd_print_str("please press sw2");//print string
+            	delay_ms(2000);//delay
          	if(GPIO_PORTF_DATA_R==0x10){break;}
 	   }
             chicken_count_down(weight); // starts a countdown with a time = 12s * weight
-            lcd_clear();
-            lcd_print_str("End Beef weight");
-            delay_ms(2000);
-            lcd_clear();
-	    led();
-	    GPIO_PORTD_DATA_R|=0X01;
-            delay_ms(2000);
-	    GPIO_PORTD_DATA_R|=0X00;
+            lcd_clear();//clear lcd
+            lcd_print_str("End Beef weight");//print String
+            delay_ms(2000);//delay
+            lcd_clear();// clear lcd
+	    led();// led in port f
+	    GPIO_PORTD_DATA_R|=0X01;//for sound
+            delay_ms(2000);//delay
+	    GPIO_PORTD_DATA_R|=0X00;//stop sound
             state = not_cooking;
             }
             else
@@ -130,9 +130,9 @@ int main() {
             while(SW1_Input() == 1){}//wait if sw1 is pressed (come from goto case)
            
             do{
-                lcd_print_str("Cooking Time?");
-                delay_ms(2000);
-                lcd_clear();
+                lcd_print_str("Cooking Time?");//print string
+                delay_ms(2000);//delay 
+                lcd_clear();//clear lcd
                 while(1){
                     input = keypad_input();
 		    delay_ms(2000);		  
@@ -156,17 +156,17 @@ int main() {
             break;
         case error_beef: //added error beef
             lcd_print_str("Err");
-	    delay_ms(2000);		  
+	    delay_ms(2000);//delay 		  
             state = beef; //get back to beef state
             break;
         case error_chicken: // added error chicken
             lcd_print_str("Err");
-	    delay_ms(2000);		  
+	    delay_ms(2000);	//delay 	  
             state = chicken; //get back to chicken state
             break;
         case error_other: // added error chicken
             lcd_print_str("Err");
-	    delay_ms(2000);		  
+	    delay_ms(2000);	//delay 	  
             state = chicken; //get back to other state
             break;
         default:
